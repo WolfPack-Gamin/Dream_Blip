1e9029b2053a26e53e6b4de278f541f7b6f5c53b
JusCampin <37031311+JusCampin@users.noreply.github.com>
2022-09-17 16:45:05 -0700

    Initial commit

---
+++ client.lua
@@ -0,0 +1,69 @@
+--local blips = {
+	--CORREOS
+  --  { name = 'Feriho farma', sprite = -758970771, x = -178.98, y = 626.72, z = 114.09 },
+	--{ name = 'Sheriff', sprite = -693644997, x = -276.28, y = 800.49, z = 119.39 },
+	--{ name = 'Zdenova krčma', sprite = 1879260108, x = -308.7, y = 804.56, z = 118,97 }
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 352.51, 1485.08, 179.53)
+    SetBlipSprite(blip, 1293773224, 1)
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Fort wallace") 
+end)
+
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 2734.8, -1114.2, 49.1)
+    SetBlipSprite(blip, 778811758, 1)
+	SetBlipScale(blip, 0.5)
+
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Le Cerf Libéré (Gazette)") 
+end)
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 1229.5, -1289.87, 76.92)
+    SetBlipSprite(blip, 778811758, 1)
+	SetBlipScale(blip, 0.5)
+
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Dream Emploi") 
+end)
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 1328.74, -1292.43, 77.01)
+    SetBlipSprite(blip, 1865251988, 1)
+	SetBlipScale(blip, 0.5)
+
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Magasin") 
+end)
+
+
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 1365.3, -1379.2, 79.3)
+    SetBlipSprite(blip, 1879260108, 1)
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Saloon") 
+end)
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 2842.39, -1234.22, 47.66)
+    SetBlipSprite(blip, 1183081869, 1)
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Marché de Saint Denis") 
+end)
+
+
+
+
+Citizen.CreateThread(function()
+    local blip = N_0x554d9d53f696d002(1664425300, 1357.8, -1309.5, 77.8)
+    SetBlipSprite(blip, 1047294027, 1)
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, "Shérif") 
+end)
+
+
+
+
+
+
+
+
+
+
